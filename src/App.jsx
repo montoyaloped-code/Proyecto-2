dbc56bebf626d7617ee600f156a10cde5bfcecc4
Sam <Montoyaloped@gmail.com>
2026-05-09 13:45:27 -0500

    Configuracion de entorno

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+export const App = () => {
+  return (
+    <>
+    
+
+
+    <h1 className='text-3xl font-bold underline text-center text-blue-500'>
+      Hello world!
+    </h1>
+    </>
+
+
+  )
+
+}
+
